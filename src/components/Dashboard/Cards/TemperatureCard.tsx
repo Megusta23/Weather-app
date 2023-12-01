2882cc11952356ce37c48c38147b41b047d292a3
Megusta23 <hasan_brkic2003@outlook.com>
2023-12-01 16:39:54 +0100

    Creating the conversion functions

--- src/components/Dashboard/Cards/TemperatureCard.tsx
+++ src/components/Dashboard/Cards/TemperatureCard.tsx
@@ -6,24 +6,30 @@ interface TemperatureProps {
 }
 
 const TemperatureCard: React.FC<TemperatureProps> = ({ temperature }) => {
-  const [isCelsius, setIsCelsius] = useState(true);
+  const [isCelsius, setIsCelsius] = useState<boolean>(true);
+  const [blackButtonColor, setBlackButtonColor] = useState<string>("#0f172a"); //black
+  const [grayButtonColor, setGrayButtonColor] = useState<string>("#cbd5e1"); //gray
 
   const convertedTemp = temperature - 273.15;
   const roundedTemperature = Math.round(convertedTemp * 100) / 100;
 
-  const handleCelsiusClick = () => {
-    setIsCelsius(true);
-  };
-
-  const handleFahrenheitClick = () => {
-    setIsCelsius(false);
+  const handleUnitToggle = (unit: string) => {
+    if (unit === "Celsius") {
+      setIsCelsius(true);
+      setBlackButtonColor("#0f172a"); //black
+      setGrayButtonColor("#cbd5e1"); //gray
+    } else if (unit === "Fahrenheit") {
+      setIsCelsius(false);
+      setBlackButtonColor("#cbd5e1"); //gray
+      setGrayButtonColor("#0f172a"); //black
+    }
   };
 
   const convertTemperature = () => {
     if (isCelsius) {
       return roundedTemperature;
     } else {
-      return (roundedTemperature * 9) / 5 + 32;
+      return Math.round(((roundedTemperature * 9) / 5 + 32) * 100) / 100;
     }
   };
 
@@ -33,31 +39,30 @@ const TemperatureCard: React.FC<TemperatureProps> = ({ temperature }) => {
         <CardTitle className="text-slate-300">Temperature:</CardTitle>
       </CardHeader>
       <CardContent className="text-center">
-        <div className="my-5 flex items-center justify-center">
+        <div className="my-5 flex items-center justify-center flex-col">
           <h1
             className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
             id="temp"
           >
             {convertTemperature()}
           </h1>
-          <div className="flex ml-2">
-            <h3
+          <div className="flex mt-5">
+            <div
               className="scroll-m-20 text-2xl font-semibold tracking-tight mr-1.5 cursor-pointer"
-              onClick={handleCelsiusClick}
+              onClick={() => handleUnitToggle("Celsius")}
+              style={{ color: blackButtonColor }}
             >
               °C
-            </h3>
-            <h3
+            </div>
+            <div
               className="scroll-m-20 text-2xl font-semibold tracking-tight text-slate-300 cursor-pointer"
-              onClick={handleFahrenheitClick}
+              onClick={() => handleUnitToggle("Fahrenheit")}
+              style={{ color: grayButtonColor }}
             >
               °F
-            </h3>
+            </div>
           </div>
         </div>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-5">
-          Caution
-        </h3>
       </CardContent>
     </Card>
   );
--- src/components/Dashboard/Cards/WindCard.tsx
+++ src/components/Dashboard/Cards/WindCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface WindProps {
@@ -6,21 +6,56 @@ interface WindProps {
 }
 
 const WindCard: React.FC<WindProps> = ({ windSpeed }) => {
+  const [isKilometer, setIsKilometer] = useState<boolean>(true);
+  const [blackButtonColor, setBlackButtonColor] = useState<string>("#0f172a"); //black
+  const [grayButtonColor, setGrayButtonColor] = useState<string>("#cbd5e1"); //gray
+
+  const kilometersPerHour = windSpeed * 3.6;
+  const roundedWindSpeed = Math.round(kilometersPerHour * 100) / 100;
+
+  const handleUnitToggle = (unit: string) => {
+    if (unit === "Kilometer") {
+      setIsKilometer(true);
+      setBlackButtonColor("#0f172a"); //black
+      setGrayButtonColor("#cbd5e1"); //gray
+    } else if (unit === "Mile") {
+      setIsKilometer(false);
+      setBlackButtonColor("#cbd5e1"); //gray
+      setGrayButtonColor("#0f172a"); //black
+    }
+  };
+
+  const convertKilometesToMiles = () => {
+    if (isKilometer) {
+      return roundedWindSpeed;
+    } else {
+      return Math.round((roundedWindSpeed / 1.609344) * 100) / 100;
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
         <CardTitle className="text-slate-300">Wind:</CardTitle>
       </CardHeader>
       <CardContent className="text-center">
-        <div className="my-5 flex items-center justify-center">
+        <div className="my-5 flex items-center justify-center flex-col">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            {windSpeed}
+            {convertKilometesToMiles()}
           </h1>
-          <div className="flex ml-2">
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mr-1.5 cursor-pointer">
+          <div className="flex mt-5">
+            <h3
+              className="scroll-m-20 text-2xl font-semibold tracking-tight mr-1.5 cursor-pointer"
+              onClick={() => handleUnitToggle("Kilometer")}
+              style={{ color: blackButtonColor }}
+            >
               km
             </h3>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-slate-300 cursor-pointer">
+            <h3
+              className="scroll-m-20 text-2xl font-semibold tracking-tight text-slate-300 cursor-pointer"
+              onClick={() => handleUnitToggle("Mile")}
+              style={{ color: grayButtonColor }}
+            >
               mi
             </h3>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight ml-2">
@@ -28,9 +63,6 @@ const WindCard: React.FC<WindProps> = ({ windSpeed }) => {
             </h3>
           </div>
         </div>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-5">
-          Light Air
-        </h3>
       </CardContent>
     </Card>
   );
